41f685a95c3c5d59d745404cfd7e08632574f9f1
Joe Bordes <joe@tsolucio.com>
2019-01-04 15:50:53 +0100

    i18n(pt_br)

--- modules/Timecontrol/language/pt_br.lang.php
+++ modules/Timecontrol/language/pt_br.lang.php
@@ -4,38 +4,35 @@
  * ("License"); You may not use this file except in compliance with the License
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
-
 Carlos Henrique de Sá Filho
 Website: https://sites.google.com/site/carlosdesaportfolio/
-
  ************************************************************************************/
 
-$mod_strings = Array(
+$mod_strings = array(
 	'Timecontrol' => 'Controle Tempo/Material',
 
 	'LBL_CUSTOM_INFORMATION' => 'Informação Personalizada',
 	'LBL_TIMECONTROL_INFORMATION' => 'Informação Tempo/Material',
 	'LBL_DESCRIPTION_INFORMATION' => 'Descrição',
-	
+
 	'SINGLE_Timecontrol'=>'Controle Tempo/Material',
-	
-	'LBL_WATCH_STOP'=>'Parar',
-	'LBL_WATCH_RESTART'=>'Iniciar',
 
-	'Timecontrol Number' => "Nº Tempo/Material",
-	'Title' => "Referência",
-	'Related Concept' => "Categoria",
-	'Date Start' => "Data Início",
-	'Time Start' => "Hora Início",
-	'Date End' => "Data Fim",
-	'Time End' => "Hora Fim",
-	'TotalTime' => "Tempo Total",
-	'Related To' => "Relacionado com",
-	'Product Name' => "Produto/Serviço",
+	'LBL_WATCH_STOP'=>'Parar Relógio',
+	'LBL_WATCH_RESTART'=>'Recomeçar Relógio',
+
+	'Timecontrol Number' => 'Nº Tempo/Material',
+	'Title' => 'Referência',
+	'Related Concept' => 'Categoria',
+	'Date Start' => 'Data de Início',
+	'Time Start' => 'Hora de Início',
+	'Date End' => 'Data de Fim',
+	'Time End' => 'Hora de Fim',
+	'TotalTime' => 'Tempo Total',
+	'Related To' => 'Relacionado com',
+	'Product Name' => 'Nome do Produto/Serviço',
 	'Units' => 'Unidades',
-	
-	'relatednum' => 'Related Num',
-	'relatedname' => 'Related Name',
-);
 
+	'relatednum' => 'Número Relacionado',
+	'relatedname' => 'Nome Relacionado',
+);
 ?>
